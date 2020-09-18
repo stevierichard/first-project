b5fe1b8bf4c983d7dae273e46daf410311132578
Steven Simon <stevierichard1@gmail.com>
2020-09-18 00:45:48 -0700

    modify Article button and card background

--- asset/app.js
+++ asset/app.js
@@ -8,16 +8,17 @@ $(document).ready(function () {
 
     for (var i = 0; i < res.articles.length; i++) {
       var col = $("<div>").addClass("col-md-4 container");
-      var card = $("<div>").addClass("card bg-info text-white");
+      var card = $("<div>").addClass("card bg-secondary text-white");
       var body = $("<div>").addClass("card-body");
       var title = $("<h5>").addClass("card-title").text(res.articles[i].title);
       var img = $("<img>").attr("src", res.articles[i].image);
       var description = $("<p>")
         .addClass("card-text")
         .text(res.articles[i].description);
-      var button = $("<button>").addClass("btn btn-secondary");
+      var button = $("<button>").addClass("btn btn-warning");
       var link = $("<a>")
-        // .attr("target", "_blank")
+        //open the page in a new tab
+        .attr("target", "_blank")
         .attr("href", res.articles[i].url)
         .text("View Article");
       button.append(link);
